369046a63d816df402faa4e8eb59d5c5e2e2c1db
lallulol <68367247+lallulol@users.noreply.github.com>
2020-07-19 13:32:27 +0300

    Update index.js
    made a small mistake  now edited

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.on("ready", () => {
     game: { name: `${client.guilds.size} Servers`, type: "STREAMING" }
   });
   client.user.setPresence({ game: { name: `${client.guilds.size} Servers`, type: "WATCHING"}});
-  client.user.setActivity("Streaming $gen|Generating Accounts!!");
+  client.user.setActivity("$gen|Generating Accounts!!");
 });
 
 client.on("message", message => {
