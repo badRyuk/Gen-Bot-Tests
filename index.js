04cd5bdf95b4a7f5c7754cf6bce86af995ee4f0f
badRyuk <68531154+badRyuk@users.noreply.github.com>
2020-10-26 10:35:29 +0300

    Updated Index.js [Command Handler, Detailed instruction]
    The bot will now use a command handler as it will make it easier to edit the bot further. Further, I have added comments in a detailed manner so that will help you to easily do stuff!

--- index.js
+++ index.js
@@ -1,277 +1,85 @@
-const Discord = require("discord.js");
-const client = new Discord.Client(); 
-// The token has been deleted long before. Below is just an example of how your token may look like...
-const token = 'NzMxNDIzMjI1NjQ0NTgwODc0.XxAbKg.2bpQC8nSbZhAgyBJq5OF6sKzRxE';
-usedCommandRecently4 = new Set();
+const Discord = require('discord.js');
 
-client.on('ready', () =>{
-  console.log('GenBot is now online')
-  client.user.setPresence({ game: { name: `${client.guilds.size} Servers`, type: "STREAMING"}});
-  client.user.setActivity("$gen|Generating Accounts!!", {
-      type: "STREAMING",
-      url: "https://www.twitch.tv/generatorbotdiscord"
-    });
-});
+const client = new Discord.Client();
 
-client.on("message", message => {
-  if (!message.guild) return;
-  if (message.content === "$gen spotify") {
-    if (usedCommandRecently4.has(message.author.id)) {
-      message.channel.send("Cooldown Message");
-    } else {
-      usedCommandRecently4.add(message.author.id);
-      setTimeout(() => {
-        usedCommandRecently4.delete(message.author.id);
-      }, 10000);
-      var string = `
-    Out Of Stock
-    Out Of Stock
-    Out Of Stock
-    Out Of Stock
-    Out Of Stock
-    Out Of Stock
-    Out Of Stock
-    Out Of Stock
-    Out Of Stock`;
-      var words = string.split("\n");
-      let random = words[Math.floor(Math.random() * words.length)];
-      message.author.send(`${random}`);
-    }
-  }
+const { MessageEmbed } = require('discord.js');
+
+const { readdirSync } = require('fs');
+
+const { join } = require('path');
+
+client.commands= new Discord.Collection();
+
+const prefix = ';';
+//You can change the prefix if you like. It doesn't have to be ;
+
+
+const commandFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(join(__dirname, "commands", `${file}`));
+    client.commands.set(command.name, command);
+}
+// This will be continously sending you status of the bot in the terminal. If you don't want it just remove the next 3 lines
+client.on("error", (e) => console.error(e));
+client.on("warn", (e) => console.warn(e));
+client.on("debug", (e) => console.info(e));
+// Till here. Delete those 3 lines if you don't want it!
+client.on("error", console.error);
+
+client.on('ready', () => {
+    console.log('I am ready');
+    client.user.setStatus(`idle`)
 });
-client.on("message", message => {
-  if (!message.guild) return;
-  if (message.content === "$gen hulu") {
-    if (usedCommandRecently4.has(message.author.id)) {
-      message.channel.send("Cooldown Message");
-    } else {
-      usedCommandRecently4.add(message.author.id);
-      setTimeout(() => {
-        usedCommandRecently4.delete(message.author.id);
-      }, 10000);
-      var string = `
-    Out Of Stock
-    Out Of Stock
-    Out Of Stock
-    Out Of Stock
-    Out Of Stock
-    Out Of Stock
-    Out Of Stock
-    Out Of Stock
-    Out Of Stock`;
-      var words = string.split("\n");
-      let random = words[Math.floor(Math.random() * words.length)];
-      message.author.send(`${random}`);
+/*
+Exporting the Commands
+*/
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+
+    if(message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+
+        const command = args.shift().toLowerCase() ||  client.aliases.get(commadName);
+
+        if(!client.commands.has(command)) return;
+
+
+        try {
+            client.commands.get(command).run(client, message, args);
+
+        } catch (error){
+            console.error(error);
+        }
     }
-  }
-});
+})
+/*
+Bot Mention
+*/
 client.on("message", message => {
-  if (!message.guild) return;
-  if (message.content === "$gen steam") {
-    if (usedCommandRecently4.has(message.author.id)) {
-      message.channel.send("Cooldown Message");
-    } else {
-      usedCommandRecently4.add(message.author.id);
-      setTimeout(() => {
-        usedCommandRecently4.delete(message.author.id);
-      }, 10000);
-      var string = `
-    LET'S GO
-    LEL
-    FUDGE
-    SHIET
-    HOLY SHIT
-    LMAO
-    DED
-    FCK
-    LOL`;
-      var words = string.split("\n");
-      let random = words[Math.floor(Math.random() * words.length)];
-      message.author.send(`${random}`);
-    }
-  }
+    if (message.author.bot) return false;
+    if (message.content.includes("@here") || message.content.includes("@everyone")) return false;
+    if(message.channel.type === 'dm') return;
+    if (message.mentions.has('760773438775492610')) { 
+     const embed = new MessageEmbed()
+        .setTitle('Hi, I\'m GenBot. Need help?')
+        .setThumbnail('https://cdn.discordapp.com/avatars/760773438775492610/a696351af5587d56907e85e97ed696cf.webp')
+        .setDescription(`You can see everything I can do by using the \`${prefix}help\` command. Remember commands won't work in DM.`)
+        .addField('Support', oneLine`
+          If you have questions, suggestions, feel free to contact a staff member!
+        `)
+        .addField('Thank You for using GenBot!', '<:Heart:764027864991268874>')
+     // Here replace the entire emoji you get
+        .setColor("RANDOM");
+        message.channel.send(embed).then(sentEmbed => {
+          // Here the bot reacts to its own message. So over here you need to insert an emoji ID so sentEmbed.react("<EMOJI_ID HERE>")
+          // If you dont want it to react then remove this part .then(sentEmbed => { sentEmbed.react("765120683428282389")
+          // If you dont know how to get emoji ID google it or send this in the server \<EMOJI>. Which will give you this result <EMOJI_NAME:EMOJI_ID>. Copy the emoji ID there and paste in the place specificed. 
+          // However, if you do the same for an animated emoji you get <a:EMOJI_NAME:EMOJI_ID>. Still problems open up an issue in the server :D
+    sentEmbed.react("765120683428282389")
+    })
+    };
 });
-client.on("message", message => {
-  if (!message.guild) return;
-  if (message.content === "$gen origin") {
-    if (usedCommandRecently4.has(message.author.id)) {
-      message.channel.send("Cooldown Message");
-    } else {
-      usedCommandRecently4.add(message.author.id);
-      setTimeout(() => {
-        usedCommandRecently4.delete(message.author.id);
-      }, 10000);
-      var string = `
-    LET'S GO
-    LEL
-    FUDGE
-    SHIET
-    HOLY SHIT
-    LMAO
-    DED
-    FCK
-    LOL`;
-      var words = string.split("\n");
-      let random = words[Math.floor(Math.random() * words.length)];
-      message.author.send(`${random}`);
-    }
-  }
-});
-client.on("message", message => {
-  if (!message.guild) return;
-  if (message.content === "$gen crunchyroll") {
-    if (usedCommandRecently4.has(message.author.id)) {
-      message.channel.send("Cooldown Message");
-    } else {
-      usedCommandRecently4.add(message.author.id);
-      setTimeout(() => {
-        usedCommandRecently4.delete(message.author.id);
-      }, 10000);
-      var string = `
-    LET'S GO
-    LEL
-    FUDGE
-    SHIET
-    HOLY SHIT
-    LMAO
-    DED
-    FCK
-    LOL`;
-      var words = string.split("\n");
-      let random = words[Math.floor(Math.random() * words.length)];
-      message.author.send(`${random}`);
-    }
-  }
-});
-client.on("message", message => {
-  if (!message.guild) return;
-  if (message.content === "$gen uplay") {
-    if (usedCommandRecently4.has(message.author.id)) {
-      message.channel.send("Cooldown Message");
-    } else {
-      usedCommandRecently4.add(message.author.id);
-      setTimeout(() => {
-        usedCommandRecently4.delete(message.author.id);
-      }, 10000);
-      var string = `
-    LET'S GO
-    LEL
-    FUDGE
-    SHIET
-    HOLY SHIT
-    LMAO
-    DED
-    FCK
-    LOL`;
-      var words = string.split("\n");
-      let random = words[Math.floor(Math.random() * words.length)];
-      message.author.send(`${random}`);
-    }
-  }
-});
-client.on("message", message => {
-  if (!message.guild) return;
-  if (message.content === "$gen roblox") {
-    if (usedCommandRecently4.has(message.author.id)) {
-      message.channel.send("Cooldown Message");
-    } else {
-      usedCommandRecently4.add(message.author.id);
-      setTimeout(() => {
-        usedCommandRecently4.delete(message.author.id);
-      }, 10000);
-      var string = `
-    LET'S GO
-    LEL
-    FUDGE
-    SHIET
-    HOLY SHIT
-    LMAO
-    DED
-    FCK
-    LOL`;
-      var words = string.split("\n");
-      let random = words[Math.floor(Math.random() * words.length)];
-      message.author.send(`${random}`);
-    }
-  }
-});
-client.on("message", message => {
-  if (!message.guild) return;
-  if (message.content === "$gen nordvpn") {
-    if (usedCommandRecently4.has(message.author.id)) {
-      message.channel.send("Cooldown Message");
-    } else {
-      usedCommandRecently4.add(message.author.id);
-      setTimeout(() => {
-        usedCommandRecently4.delete(message.author.id);
-      }, 10000);
-      var string = `
-    LET'S GO
-    LEL
-    FUDGE
-    SHIET
-    HOLY SHIT
-    LMAO
-    DED
-    FCK
-    LOL`;
-      var words = string.split("\n");
-      let random = words[Math.floor(Math.random() * words.length)];
-      message.author.send(`${random}`);
-    }
-  }
-});
-client.on("message", message => {
-  if (!message.guild) return;
-  if (message.content === "$gen minecraft") {
-    if (usedCommandRecently4.has(message.author.id)) {
-      message.channel.send("Cooldown Message");
-    } else {
-      usedCommandRecently4.add(message.author.id);
-      setTimeout(() => {
-        usedCommandRecently4.delete(message.author.id);
-      }, 10000);
-      var string = `
-    LET'S GO
-    LEL
-    FUDGE
-    SHIET
-    HOLY SHIT
-    LMAO
-    DED
-    FCK
-    LOL`;
-      var words = string.split("\n");
-      let random = words[Math.floor(Math.random() * words.length)];
-      message.author.send(`${random}`);
-    }
-  }
-});
-client.on("message", message => {
-  if (!message.guild) return;
-  if (message.content === "$gen fortnite") {
-    if (usedCommandRecently4.has(message.author.id)) {
-      message.channel.send("Cooldown Message");
-    } else {
-      usedCommandRecently4.add(message.author.id);
-      setTimeout(() => {
-        usedCommandRecently4.delete(message.author.id);
-      }, 10000);
-      var string = `
-    LET'S GO
-    LEL
-    FUDGE
-    SHIET
-    HOLY SHIT
-    LMAO
-    DED
-    FCK
-    LOL`;
-      var words = string.split("\n");
-      let random = words[Math.floor(Math.random() * words.length)];
-      message.author.send(`${random}`);
-    }
-  }
-});
-client.login(token);
-// This is old code we are updating today!
+client.login(process.env.DISCORD_TOKEN);
